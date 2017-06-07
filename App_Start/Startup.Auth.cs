b68f8da39ce197a5c286535b8f8e91bab8d7cc11
Sijun Liu <sijliu@microsoft.com>
2017-06-07 15:36:42 -0700

    initial commit

---
+++ App_Start/Startup.Auth.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.Owin.Logging;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.DataHandler.Serializer;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security.WsFederation;
+using Owin;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace KatanaSample
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            //ILogger logger = app.CreateLogger<Startup>();
+            //logger.WriteError("App is starting up");
+            //logger.WriteCritical("App is starting up");
+            //logger.WriteWarning("App is starting up");
+            //logger.WriteVerbose("App is starting up");
+            //logger.WriteInformation("App is starting up");
+
+            string authority = "https://login.microsoftonline.com/cyrano.onmicrosoft.com/";
+            string clientAddress = "http://localhost:42023/";
+            string clientId = "fe78e0b4-6fe7-47e6-812c-fb75cee266a4";
+            string oidcMetadataAddress = authority + ".well-known/openid-configuration";
+            string wsFedMetadataAddress = "https://login.microsoftonline.com/add29489-7269-41f4-8841-b63c95564420/federationmetadata/2007-06/federationmetadata.xml";
+
+            ServicePointManager.ServerCertificateValidationCallback = ValidateServerCert;
+            app.SetDefaultSignInAsAuthenticationType(OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions { AuthenticationType = OpenIdConnectAuthenticationDefaults.AuthenticationType });
+            var openIdConnectAuthenticationOptions = new OpenIdConnectAuthenticationOptions
+            {
+                AuthenticationMode = AuthenticationMode.Active,
+                AuthenticationType = OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                Authority = authority,
+                BackchannelCertificateValidator = new CustomCertifiateValidator(),
+                ClientId = clientId,
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    AuthorizationCodeReceived = (notification) => { return Task.FromResult<object>(null); },
+                    AuthenticationFailed = (notification) => 
+                    {
+                        return Task.FromResult<object>(null);
+                    },
+                    MessageReceived = (notification) => 
+                    {
+                        return Task.FromResult<object>(null);
+                    },
+                    RedirectToIdentityProvider = (notification) => {
+                        return Task.FromResult(0);
+                    },
+                    SecurityTokenReceived  = (notification) => 
+                    {
+                        return Task.FromResult<object>(null);
+                    },
+                    SecurityTokenValidated = (notification) => 
+                    {
+                        return Task.FromResult<object>(null);
+                    },
+                },
+                PostLogoutRedirectUri = string.Format(CultureInfo.InvariantCulture, "{0}Account/SignedOut", clientAddress),
+                RedirectUri = clientAddress,
+                ResponseType = OpenIdConnectResponseType.IdToken,
+            };
+
+            var handler = openIdConnectAuthenticationOptions.SecurityTokenValidator as JwtSecurityTokenHandler;
+            if (handler != null)
+            {
+                handler.InboundClaimTypeMap.Clear();
+                handler.InboundClaimFilter.Add("nonce");
+                handler.InboundClaimFilter.Add("iat");
+                handler.InboundClaimFilter.Add("exp");
+                handler.InboundClaimFilter.Add("nbf");
+            }
+            
+            app.UseOpenIdConnectAuthentication(openIdConnectAuthenticationOptions);
+            var wsFedOptions = new WsFederationAuthenticationOptions
+            {
+                AuthenticationMode = AuthenticationMode.Active,
+                AuthenticationType = WsFederationAuthenticationDefaults.AuthenticationType,
+                Notifications = new WsFederationAuthenticationNotifications
+                {
+                    MessageReceived = (notification) =>
+                    {
+                        return Task.FromResult(0);
+                    },
+                    RedirectToIdentityProvider = (notification) => {
+                        return Task.FromResult(0);
+                    },
+                    SecurityTokenValidated = (notification) =>
+                    {
+                        return Task.FromResult<object>(null);
+                    }
+                },
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    AudienceValidator = (audiences, securityToken, validationParameters) => { return true; },
+                    IssuerValidator = (issuer, securitytoken, validationParameters) => { return issuer; },
+                    SaveSigninToken = true,
+                },
+                MetadataAddress = wsFedMetadataAddress,
+                Wreply = clientAddress,
+                Wtrealm = clientId                
+            };
+
+            app.UseWsFederationAuthentication(wsFedOptions);
+        }
+
+        internal static bool ValidateServerCert(object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors error)
+        {
+          return true;
+        }
+    }
+
+    class CustomCertifiateValidator : ICertificateValidator
+    {
+        public bool Validate(Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+    }
+
+    class AuthenticationPropertiesFormater : ISecureDataFormat<AuthenticationProperties>
+    {
+        public AuthenticationPropertiesFormater()
+        {
+
+        }
+
+        public string Protect(AuthenticationProperties data)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach( var key in data.Dictionary)
+            {
+                sb.Append(key.Key);
+                sb.Append(":");
+                sb.Append(key.Value);
+            }
+
+            return sb.ToString();
+        }
+
+        public AuthenticationProperties Unprotect(string protectedText)
+        {
+            return new AuthenticationProperties();
+        }
+    }
+
+    public class StringSerializer : IDataSerializer<string>
+    {
+        public string Deserialize(byte[] data)
+        {
+            return Encoding.UTF8.GetString(data);
+        }
+
+        public byte[] Serialize(string model)
+        {
+            return Encoding.UTF8.GetBytes(model);
+        }
+    }
+
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security.WsFederation;
+
+namespace KatanaSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+                ViewBag.ClaimsPrincipal = User as ClaimsPrincipal;
+
+            return View();
+        }
+
+        public void SignOut()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (User.Identity.AuthenticationType == OpenIdConnectAuthenticationDefaults.AuthenticationType)
+                    HttpContext.GetOwinContext().Authentication.SignOut(new string[] { CookieAuthenticationDefaults.AuthenticationType, OpenIdConnectAuthenticationDefaults.AuthenticationType });
+                else if (User.Identity.AuthenticationType == WsFederationAuthenticationDefaults.AuthenticationType)
+                    HttpContext.GetOwinContext().Authentication.SignOut(new string[] { CookieAuthenticationDefaults.AuthenticationType, WsFederationAuthenticationDefaults.AuthenticationType });
+            }
+        }
+
+        public void SignInOIDC()
+        {
+            HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" }, OpenIdConnectAuthenticationDefaults.AuthenticationType);
+        }
+
+        public void SignInWsFed()
+        {
+            HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" }, WsFederationAuthenticationDefaults.AuthenticationType);
+        }
+
+        public ActionResult SignedOut()
+        {
+            SignOut();
+            return View();
+        }
+    }
+}
